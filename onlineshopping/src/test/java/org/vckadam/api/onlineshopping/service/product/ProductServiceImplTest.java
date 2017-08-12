d5accf4dd7728689db5c36d2c70849862168f27d
vckadam <vckadam@asu.edu>
2017-08-11 23:41:18 -0400

    Tested getTopCatIds

--- onlineshopping/src/test/java/org/vckadam/api/onlineshopping/service/product/ProductServiceImplTest.java
+++ onlineshopping/src/test/java/org/vckadam/api/onlineshopping/service/product/ProductServiceImplTest.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.Test;
 import org.vckadam.api.onlineshopping.model.product.Product;
 import org.vckadam.api.onlineshopping.model.product.ProductPurchasedByUser;
 import org.vckadam.api.onlineshopping.model.user.User;
@@ -27,6 +28,7 @@ public class ProductServiceImplTest extends TestCase {
 		this.productServiceImpl = null;
 	}
 
+	@Test
 	public void testGetPurchasedProducts_BasicScenario() {
 		Long[] userIds = {1L,2L,3L};
 		List<User> users = prepUserListForGetPurchasedProducts(userIds);
@@ -44,6 +46,7 @@ public class ProductServiceImplTest extends TestCase {
 		assertEquals(userToProdExp, userToProdAct);
 	}
 	
+	@Test
 	public void testGetPurchasedProducts_ExtraUser() {
 		Long[] userIds = {1L,2L,3L,4L}; //4L user is extra
 		List<User> users = prepUserListForGetPurchasedProducts(userIds);
@@ -61,6 +64,7 @@ public class ProductServiceImplTest extends TestCase {
 		assertEquals(userToProdExp, userToProdAct);
 	}
 	
+	@Test
 	public void testGetPurchasedProducts_ExtraProduct() {
 		Long[] userIds = {1L,2L,3L,4L}; 
 		List<User> users = prepUserListForGetPurchasedProducts(userIds);
@@ -78,6 +82,7 @@ public class ProductServiceImplTest extends TestCase {
 		assertEquals(userToProdExp, userToProdAct);
 	}
 	
+	@Test
 	public void testGetPurchasedProducts_ExtraUserHistory() {
 		Long[] userIds = {1L,2L,3L,4L}; 
 		List<User> users = prepUserListForGetPurchasedProducts(userIds);
@@ -95,6 +100,7 @@ public class ProductServiceImplTest extends TestCase {
 		assertEquals(userToProdExp, userToProdAct);
 	}
 	
+	@Test
 	public void testGetPurchasedProducts_NullUser() {
 		Long[] userIds = {null,1L,2L,null,3L,4L}; 
 		List<User> users = prepUserListForGetPurchasedProducts(userIds);
@@ -112,6 +118,7 @@ public class ProductServiceImplTest extends TestCase {
 		assertEquals(userToProdExp, userToProdAct);
 	}
 	
+	@Test
 	public void testGetPurchasedProducts_NullProduct() {
 		Long[] userIds = {null,1L,2L,null,3L,4L}; 
 		List<User> users = prepUserListForGetPurchasedProducts(userIds);
@@ -129,6 +136,7 @@ public class ProductServiceImplTest extends TestCase {
 		assertEquals(userToProdExp, userToProdAct);
 	}
 	
+	@Test
 	public void testGetPurchasedProducts_NullUserHistory() {
 		Long[] userIds = {null,1L,2L,null,3L,4L}; 
 		List<User> users = prepUserListForGetPurchasedProducts(userIds);
@@ -146,6 +154,7 @@ public class ProductServiceImplTest extends TestCase {
 		assertEquals(userToProdExp, userToProdAct);
 	}
 	
+	@Test
 	public void testGetPurchasedProducts_DuplicateUserHistory() {
 		Long[] userIds = {null,1L,2L,null,3L,4L}; 
 		List<User> users = prepUserListForGetPurchasedProducts(userIds);
@@ -221,5 +230,31 @@ public class ProductServiceImplTest extends TestCase {
 			userProdsId.put((Long)ele[0], (List<Long>)ele[1]);
 		}
 		return userProdsId;
+	}
+	
+	/*********************************************************************************************/
+	
+	@Test
+	public void testGetTopCatIds() {
+		Long[][] prodCatId = {{1L,1L},{2L,1L},{3L,2L}};
+		List<Product> prods = creatProdListForGetTopCatIds(prodCatId);
+		List<Long> actCatIds = this.productServiceImpl.getTopCatIds(prods);
+		List<Long> expCatIds = new ArrayList<Long>(Arrays.asList(1L,2L));
+		assertEquals(expCatIds, actCatIds);
+	}
+	
+	private List<Product> creatProdListForGetTopCatIds(Long[][] prodCatIds) {
+		List<Product> prods = new ArrayList<Product>();
+		for(Long[] prodCatId : prodCatIds) {
+			prods.add(new Product(prodCatId[0], null, prodCatId[1], null));
+		}
+		return prods;
+	}
+	
+	/**********************************************************************************************/
+	
+	@Test
+	public void testGetTopUserCategories() {
+		
 	}
 }
